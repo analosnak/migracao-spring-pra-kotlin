0c1c6a2f4a7de01068aacb3353995085a40a9538
Ana Losnak <ana.losnak@caelum.com.br>
2019-05-23 15:28:35 -0300

    Ex 11.4 - agendando a tarefa

--- src/main/java/br/com/alura/forum/AluraForumApplication.java
+++ src/main/java/br/com/alura/forum/AluraForumApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @EnableAsync
 @EnableSpringDataWebSupport
 @SpringBootApplication
