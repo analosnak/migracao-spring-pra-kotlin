e854bc790d326d7c03b0cd092fa41e11f7d18f63
Ana Losnak <ana.losnak@caelum.com.br>
2019-05-24 14:43:29 -0300

    Ex 11.6 - Criando lógica pra mostrar relatório de tópicos abertos

--- src/main/java/br/com/alura/forum/repository/OpenTopicByCategoryRepository.java
+++ src/main/java/br/com/alura/forum/repository/OpenTopicByCategoryRepository.java
@@ -1,9 +1,17 @@
 package br.com.alura.forum.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 
 import br.com.alura.forum.model.OpenTopicByCategory;
 
 public interface OpenTopicByCategoryRepository extends Repository<OpenTopicByCategory, Long> {
 	void saveAll(Iterable<OpenTopicByCategory> topics);
+
+	@Query("select t from OpenTopicByCategory t " +
+            "where year(t.date) = year(current_date) " +
+            "and month(t.date) = month(current_date)")
+	List<OpenTopicByCategory> findAllByCurrentMonth();
 }
--- src/main/java/br/com/alura/forum/security/configuration/SecurityConfiguration.java
+++ src/main/java/br/com/alura/forum/security/configuration/SecurityConfiguration.java
@@ -74,7 +74,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter  {
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring().antMatchers("/**.html",  "/v2/api-docs", "/webjars/**", 
-				"/configuration/**", "/swagger-resources/**");
+				"/configuration/**", "/swagger-resources/**", "/css/**",
+                "/**.ico", "/js/**");
 	}
 	
 	private static class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
