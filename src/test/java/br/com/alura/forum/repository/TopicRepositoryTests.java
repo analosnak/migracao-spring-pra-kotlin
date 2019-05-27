cd7fbf3fc25cb8b92ec7a0bc33a3adb451252553
Ana Losnak <ana.losnak@caelum.com.br>
2019-05-27 00:51:46 -0300

    Ex 12.4 - Usando MySql de produção pra teste

--- src/test/java/br/com/alura/forum/repository/TopicRepositoryTests.java
+++ src/test/java/br/com/alura/forum/repository/TopicRepositoryTests.java
@@ -7,6 +7,8 @@ import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -16,6 +18,7 @@ import br.com.alura.forum.repository.setup.TopicRepositoryTestsSetup;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 public class TopicRepositoryTests {
 	@Autowired
 	private TopicRepository topicRepository;
