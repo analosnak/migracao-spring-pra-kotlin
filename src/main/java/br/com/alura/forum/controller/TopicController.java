cc1ea81b845b8f9e744805dba356db96d4a2b2eb
Ana Losnak <ana.losnak@caelum.com.br>
2019-12-18 16:04:45 -0300

    migrando endpoint de criar tópico pra Kotlin

--- src/main/java/br/com/alura/forum/controller/TopicController.java
+++ src/main/java/br/com/alura/forum/controller/TopicController.java
@@ -1,48 +1,20 @@
 package br.com.alura.forum.controller;
 
-import java.net.URI;
 import java.util.List;
-
-import javax.validation.Valid;
-
-import br.com.alura.forum.controller.dto.output.TopicOutputDto;
-import br.com.alura.forum.model.User;
-import br.com.alura.forum.model.topic.domain.Topic;
-import br.com.alura.forum.repository.TopicRepository;
-import br.com.alura.forum.validator.NewTopicCustomValidator;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.domain.Specification;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import br.com.alura.forum.controller.dto.input.NewTopicInputDto;
-import br.com.alura.forum.controller.dto.input.TopicSearchInputDto;
-import br.com.alura.forum.controller.dto.output.TopicBriefOutputDto;
 import br.com.alura.forum.controller.dto.output.TopicDashboardItemOutputDto;
-import br.com.alura.forum.repository.CourseRepository;
 import br.com.alura.forum.service.DashboardDataProcessingService;
 import br.com.alura.forum.vo.CategoriesAndTheirStatisticsData;
 
 @RestController
 @RequestMapping("/api/topics")
 public class TopicController {
-
-	@Autowired
-	private TopicRepository topicRepository;
 	
 	@Autowired
 	private DashboardDataProcessingService dashboardDataProcessingService;
 
-	@Autowired
-	private CourseRepository courseRepository;
  
     @GetMapping(value = "/dashboard", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<TopicDashboardItemOutputDto> getDashboardInfo() {
@@ -50,24 +22,6 @@ public class TopicController {
         CategoriesAndTheirStatisticsData categoriesStatisticsData = this.dashboardDataProcessingService.execute();
         return TopicDashboardItemOutputDto.listFromCategories(categoriesStatisticsData);
     		
-    }
-    
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<TopicOutputDto> createTopic(@RequestBody @Valid NewTopicInputDto newTopicDto,
-                                                      @AuthenticationPrincipal User loggedUser, UriComponentsBuilder uriBuilder) {
-
-        Topic topic = newTopicDto.build(loggedUser, this.courseRepository);
-        this.topicRepository.save(topic);
-
-        URI path = uriBuilder.path("/api/topics/{id}")
-                .buildAndExpand(topic.getId()).toUri();
-
-        return ResponseEntity.created(path).body(new TopicOutputDto(topic));
-	}
-
-	@InitBinder("newTopicInputDto")
-	public void initBinder(WebDataBinder binder, @AuthenticationPrincipal User loggedUser) {
-        binder.addValidators(new NewTopicCustomValidator(this.topicRepository, loggedUser));
     }
 
 }
--- src/main/kotlin/br/com/alura/forum/controller/TopicControllerKotlin.kt
+++ src/main/kotlin/br/com/alura/forum/controller/TopicControllerKotlin.kt
@@ -1,24 +1,30 @@
 package br.com.alura.forum.controller
 
+import br.com.alura.forum.controller.dto.input.NewTopicInputDto
 import br.com.alura.forum.controller.dto.input.TopicSearchInputDto
 import br.com.alura.forum.controller.dto.output.TopicBriefOutputDto
 import br.com.alura.forum.controller.dto.output.TopicOutputDto
+import br.com.alura.forum.model.User
 import br.com.alura.forum.model.topic.domain.Topic
+import br.com.alura.forum.repository.CourseRepository
 import br.com.alura.forum.repository.TopicRepository
+import br.com.alura.forum.validator.NewTopicCustomValidator
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
 import org.springframework.data.jpa.domain.Specification
 import org.springframework.data.web.PageableDefault
 import org.springframework.http.MediaType
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.WebDataBinder
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.util.UriComponentsBuilder
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("/api/topics")
-class TopicControllerKotlin(val topicRepository: TopicRepository) {
+class TopicControllerKotlin(val topicRepository: TopicRepository, val courseRepository: CourseRepository) {
 	@GetMapping(value = ["/{id}"], produces = [MediaType.APPLICATION_JSON_VALUE])
 	fun getTopicDetails(@PathVariable id: Long) = TopicOutputDto(this.topicRepository.findById(id))
 
@@ -31,5 +37,23 @@ class TopicControllerKotlin(val topicRepository: TopicRepository) {
 		val topicsPage = this.topicRepository.findAll(topicSearchSpecification, pageRequest)
 
 		return TopicBriefOutputDto.listFromTopics(topicsPage)
+	}
+
+	@PostMapping(consumes = [MediaType.APPLICATION_JSON_VALUE], produces = [MediaType.APPLICATION_JSON_VALUE])
+	fun createTopic(@RequestBody @Valid newTopicDto: NewTopicInputDto,
+					@AuthenticationPrincipal loggedUser: User,
+					uriBuilder: UriComponentsBuilder): ResponseEntity<TopicOutputDto> {
+		val topic = newTopicDto.build(loggedUser, this.courseRepository)
+		this.topicRepository.save(topic)
+
+		val path = uriBuilder.path("/api/topics/{id}")
+				.buildAndExpand(topic.id).toUri()
+
+		return ResponseEntity.created(path).body(TopicOutputDto(topic))
+	}
+
+	@InitBinder("newTopicInputDto")
+	fun initBinder(binder: WebDataBinder, @AuthenticationPrincipal loggedUser: User) {
+		binder.addValidators(NewTopicCustomValidator(this.topicRepository, loggedUser))
 	}
 }
