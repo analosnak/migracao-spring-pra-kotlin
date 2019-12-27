b63b3ce2b393eb50a0aafcbc53f279ac339d4b6b
Ana Losnak <ana.losnak@caelum.com.br>
2019-12-26 23:34:27 -0300

    Migrando CategoryStatisticsDataLoadingService pra kotlin

---
+++ src/main/kotlin/br/com/alura/forum/service/CategoryStatisticsDataLoadingService.kt
@@ -0,0 +1,28 @@
+package br.com.alura.forum.service
+
+import br.com.alura.forum.model.Category
+import br.com.alura.forum.repository.TopicRepository
+import br.com.alura.forum.vo.CategoryStatisticsData
+import org.springframework.stereotype.Service
+import java.time.Instant
+import java.time.temporal.ChronoUnit
+
+@Service
+class CategoryStatisticsDataLoadingService(private val topicRepository: TopicRepository) {
+
+    fun load(category: Category): CategoryStatisticsData {
+        category.id?.let {
+        val allTopics = this.topicRepository.countTopicsByCategoryId(category.id)
+
+        val lastWeek = Instant.now().minus(7, ChronoUnit.DAYS)
+        val lastWeekTopics = this.topicRepository
+                .countLastWeekTopicsByCategoryId(category.id, lastWeek)
+
+        val unansweredTopics = this.topicRepository
+                .countUnansweredTopicsByCategoryId(category.id)
+
+            return CategoryStatisticsData(allTopics, lastWeekTopics, unansweredTopics)
+        }
+        throw IllegalStateException("O id da Categoria não pode ser nulo nesse momento")
+    }
+}
